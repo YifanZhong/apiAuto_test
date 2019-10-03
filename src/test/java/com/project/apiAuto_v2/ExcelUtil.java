b43e10fd94285651b5cf8467aaee799ff25f5d88
Ivan Zhong <ivanzhong0310@gmail.com>
2019-10-04 00:47:08 +1300

    input test case from Excel using WorkbookFactory

---
+++ src/test/java/com/project/apiAuto_v2/ExcelUtil.java
@@ -0,0 +1,49 @@
+package com.project.apiAuto_v2;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ExcelUtil {
+    public static Object [][] testdata(){
+        String excelPath = "src/test/resources/TestCase_v1.xlsx";
+        //获取Workbook对象
+        Object[][] testdata = null;
+        try {
+            Workbook workbook = WorkbookFactory.create(new File(excelPath));
+            //获取Sheet对象
+            Sheet sheet = workbook.getSheet("RegisterTestCaseSheet");
+            //获取行
+            testdata = new Object[6][2];
+            for (int i = 1; i <= 6; i++){
+                Row row = sheet.getRow(i);
+                for (int j = 5; j <= 6; j++){
+                    Cell cell = row.getCell(j);
+                    //将列设置为字符串类型
+                    cell.setCellType(CellType.STRING);
+                    String cellValue = cell.getStringCellValue();
+                    testdata[i-1][j-5] = cellValue;
+                }
+            }
+
+            //获取列
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InvalidFormatException e) {
+            e.printStackTrace();
+        }
+
+        return testdata;
+    }
+
+    public static void main(String[] args){
+        Object[][] testdata = testdata();
+        for (Object[] objects : testdata){
+            for (Object object:objects){
+                System.out.println("["+object+"]");
+            }
+        }
+    }
+}
---
+++ src/test/java/com/project/apiAuto_v2/RegisterCase_v3.java
@@ -0,0 +1,42 @@
+package com.project.apiAuto_v2;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterCase_v3 {
+    //mobilephone:"13644445555", pwd:""
+    //mobilephone:"", pwd:"123456"
+    //mobilephone:"1364444", pwd:"123456"
+    //mobilephone:"13644445555", pwd:"12345"
+    //mobilephone:"13644445555", pwd:"123456"
+    //mobilephone:"13644445555", pwd:"123456"
+
+
+    @Test(dataProvider = "testdata")
+    public void Test1(String mobilephone, String pwd) {
+        String url = "http://47.107.166.132:8080/futureloan/mvc/api/member/register";
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("mobilephone", mobilephone);
+        params.put("pwd", pwd);
+        System.out.println(HttpUtil.doPost(url, params));
+    }
+
+
+    @DataProvider
+    public Object[][] testdata(){
+        Object [][] testdata = {
+                {"13655554444",""},
+                {"","123456"},
+                {"1365555","123456"},
+                {"13655554444","12345"},
+                {"13655554444","123456"},
+                {"13655554444","123456"},
+
+        };
+        return testdata;
+    }
+
+}
