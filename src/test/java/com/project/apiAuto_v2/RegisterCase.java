f014ca08676b77d56175841abf934dd574a33000
Ivan Zhong <ivanzhong0310@gmail.com>
2019-10-03 21:57:47 +1300

    applied TestNG in project

--- src/test/java/com/project/apiAuto_v2/RegisterCase.java
+++ src/test/java/com/project/apiAuto_v2/RegisterCase.java
@@ -1,4 +1,18 @@
 package com.project.apiAuto_v2;
 
+import org.testng.annotations.Test;
+
 public class RegisterCase {
+    //mobilephone:"13644445555", pwd:""
+    //mobilephone:"", pwd:"123456"
+    //mobilephone:"1364444", pwd:"123456"
+    //mobilephone:"13644445555", pwd:"12345"
+    //mobilephone:"13644445555", pwd:"123456"
+    //mobilephone:"13644445555", pwd:"123456"
+
+
+    @Test
+    public void Test1(){
+        System.out.println("This is test 1");
+    }
 }
