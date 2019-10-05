936026add5d7c20c39b78c74b4e2723fd68ce8f9
Ivan Zhong <ivanzhong0310@gmail.com>
2019-10-05 16:30:47 +1300

    run LoginCase_v8

---
+++ src/test/java/com/project/apiAuto_v6/LoginCase_v8.java
@@ -0,0 +1,38 @@
+package com.project.apiAuto_v6;
+
+import com.alibaba.fastjson.JSONObject;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.Map;
+
+public class LoginCase_v8 {
+
+    @Test(dataProvider = "testdata")
+    public void Test1(String apiId, String parameters) {
+        //url
+        String url = RestUtil.getUrlByApiId(apiId);
+        //type
+        String type = RestUtil.getTypeByApiId(apiId);
+        //所需参数
+        Map<String, String> params = (Map<String, String>) JSONObject.parse(parameters);
+        String result = HttpUtil.doService(url,type,params);
+        /**
+         if ("POST".equalsIgnoreCase(type)){
+         HttpUtil.doPost(url, params);
+         }else if ("GET".equalsIgnoreCase(type)){
+         HttpUtil.doGet(url,params);
+         }
+         **/
+        System.out.println(result);
+
+    }
+
+
+    @DataProvider
+    public Object[][] testdata(){
+        String [] cellNames = {"ApiId","Params"};
+        Object [][] testdata = CaseUtil.getCaseDatasByApiId("2",cellNames);
+        return testdata;
+    }
+}
